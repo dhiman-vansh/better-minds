f8b24af475ec85c6bcead66e0dd89343b840895a
Vansh <vansh8755381346@gmail.com>
2021-11-01 00:09:19 +0530

    in

--- src/comp/homepg/Ytcomp.jsx
+++ src/comp/homepg/Ytcomp.jsx
@@ -5,13 +5,13 @@ import "./Style.css"
 export default function Ytcomp() {
     return(
         <div className="ytcomp">
-        <div>
+        
         <img src="https://s3.envato.com/files/273747259/349_T13A4803.jpg"
         className="ytcompimg" />
-        </div>
+        
         <div >
         <ReactPlayer controls
-        width='370px'
+        width='100%'
         url="https://www.youtube.com/watch?v=zTitoHKsyJg" />
         </div>
         </div>
