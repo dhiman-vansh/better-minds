17f0122d4ec0426f15d8e87c9ee4dc47122b8816
Vansh <vansh8755381346@gmail.com>
2021-10-31 16:17:44 +0530

    init

--- src/comp/Home.jsx
+++ src/comp/Home.jsx
@@ -8,6 +8,7 @@ import Why from "./homepg/Why";
 export default function Home() {
     return(
         <div id="home">
+            hi
         <h2 className="headingsbtw" id="slider">Our Results</h2>
         <Slidecomp />
         <h2 className="headingsbtw" id="yt">Youtube Channel</h2>
