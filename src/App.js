67cb5abfaf51ca4d752715fac8a3a13f88607540
Vansh <vansh8755381346@gmail.com>
2022-03-24 18:14:11 +0530

    login

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Navcomp from "./comp/Navcomp";
 import Footercomp from "./comp/Footercomp";
 import Home from "./comp/Home";
 import {Switch, Route} from 'react-router-dom'
+import Login from "./comp/Login";
 
 function App() {
   return (
   <>
   <Navcomp  />
   <Switch>
-    <Route path='/' component={Home} /> 
+    <Route exact path='/' component={Home} /> 
+    <Route path='/login' component={Login} /> 
   </ Switch>
   <Footercomp />
   </>
--- src/comp/Navcomp.jsx
+++ src/comp/Navcomp.jsx
@@ -1,9 +1,12 @@
 import 'react-bootstrap'
 import './All.css'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react';
 import { Nav, Card, Button, Navbar, NavDropdown, Container, NavbarBrand } from 'react-bootstrap'
 
 export default function Navcomp() {
+
+  // const icon = <FontAwesomeIcon icon={faCoffee} />
     const [show, setShow] = useState(false);
 const showDropdown = (e)=>{
     setShow(!show);
@@ -19,7 +22,7 @@ const hideDropdown = e => {
     <img src="https://raw.githubusercontent.com/dhiman-vansh/Coding/master/Better%20Minds%20Logo.JPG"
     width='70' className='imgnav' />
   </NavbarBrand>
-  <Navbar.Brand href="#home"><strong>Better Minds</strong></Navbar.Brand>
+  <Navbar.Brand href="/"><strong>Better Minds</strong></Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
@@ -45,6 +48,7 @@ const hideDropdown = e => {
       </Nav.Link>
     </Nav>
   </Navbar.Collapse>
+  <Navbar.Brand href="/login" ><strong>Login</strong></Navbar.Brand>
   </Container>
 </Navbar>
        </>
